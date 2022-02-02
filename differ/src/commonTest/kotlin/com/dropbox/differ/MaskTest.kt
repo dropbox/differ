1333614676ddb966055bfd9587a1a62d8ea58956
Ryan Harter <rharter@dropbox.com>
2022-02-02 12:14:26 -0600

    Adds first mask test.

---
+++ differ/src/commonTest/kotlin/com/dropbox/differ/MaskTest.kt
@@ -0,0 +1,11 @@
+package com.dropbox.differ
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class MaskTest {
+  @Test fun `starts with an empty map`() {
+    val mask = Mask(100, 100)
+    assertEquals(0.0, mask.difference)
+  }
+}
